99853fd4b0ef5039c20f0b236c9cde21ece17fb9
ibrahimelaji00 <ibrahimelaji00@gmail.com>
2025-09-11 10:09:12 +0200

    Add function-based and class-based views with templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,21 @@
-from .models import Library, Book
 from django.shortcuts import render
 from django.views.generic import DetailView
+from .models import Library, Book
 
-# Function-based view for listing books
+
 # Function-based view for listing all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 # Class-based view for a library detail
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+
+# Function-based view for listing books
+# Function-based view for listing all books
+
