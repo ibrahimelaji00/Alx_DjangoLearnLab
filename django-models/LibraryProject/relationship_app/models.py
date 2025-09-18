a476300c17e629faac67f3d5673b6054453fbff1
ibrahimelaji00 <ibrahimelaji00@gmail.com>
2025-09-18 05:49:38 +0100

    Implement custom permissions for Book model (add, edit, delete)

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -56,3 +56,19 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     else:
         instance.userprofile.save()
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,3 +16,11 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("add-book/", views.add_book, name="add_book"),
+    path("edit-book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("delete-book/<int:book_id>/", views.delete_book, name="delete_book"),
+]
